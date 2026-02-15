08fa6990d8d91282e5ed8eee2e247b12ea61080c
nadymahmoud <asmaylsmt70@gmail.com>
2026-02-15 14:40:41 +0200

    design onboarding page part 1 add fonts

---
+++ lib/core/font_manager.dart
@@ -0,0 +1,4 @@
+class FontManager{
+  static const String orbitronName = 'Orbitron';
+  static const String orbitronPathReguler = 'assets/fonts/Orbitron-Regular.ttf';
+}
--- lib/features/on_boarding/screens/on_boarding_screens.dart
+++ lib/features/on_boarding/screens/on_boarding_screens.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/core/font_manager.dart';
+
+import '../../../core/alignment_manager.dart';
+import '../../../core/color_manager.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin:
+                  Alignment(AlignmentManagerX.x0_20, AlignmentManagerY.x_0_98),
+              end: Alignment(AlignmentManagerX.x_0_20, AlignmentManagerY.x0_98),
+              colors: [
+                ColorManager.kPrimaryColor,
+                ColorManager.kSecondPrimaryColor.withOpacity(0.4)
+              ]),
+        ),
+        child: Center(
+          child: Text(
+            "OnBoarding",
+            style: TextStyle(fontFamily: FontManager.orbitronName),
+          ),
+        ),
+      ),
+    );
   }
 }
