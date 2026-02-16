e59f4cf1c8b63600b86e82dba81120f1b7abe3c1
nadymahmoud <asmaylsmt70@gmail.com>
2026-02-16 13:31:57 +0200

    design home page part 2 make bottom navigation bar

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -5,6 +5,21 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.red,
+          items: [
+            BottomNavigationBarItem(
+                label: "",
+                icon: Image(
+                    image: AssetImage(
+                        "assets/images/button_nav_bar/left_alignment.png"))),
+            BottomNavigationBarItem(label: "", icon: Icon(Icons.home)),
+            BottomNavigationBarItem(label: "", icon: Icon(Icons.heart_broken)),
+            BottomNavigationBarItem(label: "", icon: Icon(Icons.list)),
+            BottomNavigationBarItem(label: "", icon: Icon(Icons.settings)),
+          ]),
+    );
   }
 }
